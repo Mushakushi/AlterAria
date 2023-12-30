615a5bdc66f30f80130f8feb4a7ff908b2142083
Matthew Brown <matthewbrown.classes@gmail.com>
2023-12-30 14:15:16 -0500

    changed source organization

--- Source/Character/Public/BaseCharacterComponent.h
+++ Source/Character/Public/BaseCharacterComponent.h
@@ -3,7 +3,6 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "GameFramework/Character.h"
 #include "Components/PawnComponent.h"
 #include "Logging/LogMacros.h"
 #include "BaseCharacterComponent.generated.h"
